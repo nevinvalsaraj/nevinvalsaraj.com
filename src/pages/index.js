ed64408005af10fb955c1ccfe258463e223aefed
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2019-03-16 19:14:02 +0530

    Add github link
    Add github link.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,9 @@ const IndexPage = () => (
     <h1>
       ~ <Link to="projects">projects</Link>
     </h1>
+    <h1>
+      ~ <a href="https://github.com/nevinvalsaraj">github</a>
+    </h1>
   </div>
 )
 
