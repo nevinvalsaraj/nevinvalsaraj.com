57f94edc8105a7cff915e4daf17eddbc228a52f9
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2020-07-26 16:19:11 +0530

    Updates

--- path----2b97f5244fadc41f2870.js
+++ path----2b97f5244fadc41f2870.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"~ / nevin valsaraj"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{101:function(t,a){t.exports={data:{site:{siteMetadata:{title:"~ / nevin valsaraj"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----2b97f5244fadc41f2870.js.map
