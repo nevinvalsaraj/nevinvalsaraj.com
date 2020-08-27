0e468eb2e2f0bdac2b55307c5ace021dd4d0203e
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2020-08-28 01:30:31 +0530

    Add site header
    Add site header component.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Proza+Libre:wght@600&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Proza Libre', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="siteHeader">
+    <ul>
+      <h1>~/&nbsp;</h1>
+      <h1>nevin&nbsp;</h1>
+      <h1>valsaraj</h1>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+      return {
+          siteName: '~ / nevin valsaraj'
+      }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.siteHeader {
+  background: lightseagreen;
+  background: linear-gradient(
+    42deg,
+    rgba(32, 223, 154, 1) 0%,
+    rgba(32, 178, 170, 1) 100%
+  );
+  min-height: 25rem;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 2rem;
+}
+
+.siteHeader > ul {
+  display: flex;
+  flex-direction: row;
+  padding: 0 0;
+}
+
+.siteHeader > ul > h1 {
+  margin: 0 0;
+  font-size: 7rem;
+}
+
+@media only screen and (max-width: 600px) {
+  .siteHeader > ul {
+    flex-direction: column;
+  }
+
+  .siteHeader > ul > h1 {
+    font-size: 4rem;
+  }
+}
+
+</style>
